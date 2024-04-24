c1469acd6897bf899a6707ce1b9f57c92ee64ead
Simone Taliano <talia4343@gmail.com>
2024-04-24 14:57:33 +0200

    listitem

--- src/CorrectAnswers.jsx
+++ src/CorrectAnswers.jsx
@@ -6,36 +6,43 @@ function CorrectAnswers({ correctAnswers, userAnswers }) {
   return (
     <Box className="correct-answers" sx={{ width: '75%', margin: 'auto' }}>
       <Typography variant="h5">Risposte Corrette:</Typography>
-        <ListItem>
-          <ListItemText
-            primary={
-              <span style={{ color: correctAnswers.name !== userAnswers.name ? 'red' : 'inherit' }}>
-                Nome: {correctAnswers.name}
-              </span>
-            }
-          />
-          <ListItemText
-            primary={
-              <span style={{ color: correctAnswers.author !== userAnswers.author ? 'red' : 'inherit' }}>
-                Autore: {correctAnswers.author}
-              </span>
-            }
-          />
-          <ListItemText
-            primary={
-              <span style={{ color: correctAnswers.date !== userAnswers.date ? 'red' : 'inherit' }}>
-                Data: {correctAnswers.date}
-              </span>
-            }
-          />
+      <ListItem>
+        <ListItemText
+          primary={
+            <span style={{ color: correctAnswers.name !== userAnswers.name ? 'red' : 'inherit' }}>
+              Nome: {correctAnswers.name}
+            </span>
+          }
+        />
+        <ListItemText
+          primary={
+            <span style={{ color: correctAnswers.author !== userAnswers.author ? 'red' : 'inherit' }}>
+              Autore: {correctAnswers.author}
+            </span>
+          }
+        />
+        <ListItemText
+          primary={
+            <span style={{ color: correctAnswers.date !== userAnswers.date ? 'red' : 'inherit' }}>
+              Data: {correctAnswers.date}
+            </span>
+          }
+        />
+        <ListItemText
+          primary={
+            <span style={{ color: correctAnswers.context !== userAnswers.context ? 'red' : 'inherit' }}>
+              Contesto: {correctAnswers.context}
+            </span>
+          }
+        />
+        <ListItemText
+          primary={
+            <span style={{ color: correctAnswers.place !== userAnswers.place ? 'red' : 'inherit' }}>
+              Posto: {correctAnswers.place}
+            </span>
+          }
+        />
       </ListItem>
-      <ul>
-        <li color='red'>Nome: {correctAnswers.name}</li>
-        <li>Autore: {correctAnswers.author}</li>
-        <li>Data: {correctAnswers.date}</li>
-        <li>Contesto: {correctAnswers.context}</li>
-        <li>Posto: {correctAnswers.place ? correctAnswers.place : '-'}</li>
-      </ul>
     </Box>
   );
 }
